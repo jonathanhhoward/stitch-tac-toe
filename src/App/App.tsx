961c1215d5693e3b4199030f71f22a12ef308140
Jonathan Howard <jonathanhoward@fastmail.com>
2024-02-22 11:28:58 -0500

    Use barreling

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { AppContext } from "./AppContext";
-import Board from "./components/Board/Board";
-import Header from "./components/Header/Header";
+import { Board } from "./components/Board";
+import { Header } from "./components/Header";
 import { useStitchTacToe } from "./hooks/useStitchTacToe";
 
 export default function App() {
