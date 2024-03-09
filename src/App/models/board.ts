bf4051195e1e19446914b646e7d8fbec0b1e5ced
Jonathan Howard <jonathanhoward@fastmail.com>
2024-03-08 21:19:21 -0500

    Use class methods

--- src/App/models/board.ts
+++ src/App/models/board.ts
@@ -4,17 +4,19 @@ import { Coordinate, Player, PlayerGrid } from "../types";
 export class Board {
   private constructor(public readonly grid: PlayerGrid) {}
 
-  static create = (): Board =>
-    new Board(Array(3).fill(Array(3).fill(emptyPlayer)));
+  static create(): Board {
+    return new Board(Array(3).fill(Array(3).fill(emptyPlayer)));
+  }
 
-  add = (player: Player, [row, col]: Coordinate): Board =>
-    new Board(
+  add(player: Player, [row, col]: Coordinate): Board {
+    return new Board(
       this.grid.map((boardRow, iRow) =>
         boardRow.map((square, iCol) =>
           row === iRow && col === iCol ? player : square,
         ),
       ),
     );
+  }
 
   checkForWinner(): Player {
     for (let row = 0; row < 3; ++row) {
@@ -40,32 +42,57 @@ export class Board {
     return emptyPlayer;
   }
 
-  isOccupiedAt = ([row, col]: Coordinate): boolean =>
-    !!this.grid[row][col].name;
+  isOccupiedAt([row, col]: Coordinate): boolean {
+    return !!this.grid[row][col].name;
+  }
 
-  private isWinnerInRow = (row: number) =>
-    this.isThreeInARow(this.grid[row][0], this.grid[row][1], this.grid[row][2]);
+  private isWinnerInRow(row: number) {
+    return this.isThreeInARow(
+      this.grid[row][0],
+      this.grid[row][1],
+      this.grid[row][2],
+    );
+  }
 
-  private isWinnerInColumn = (col: number) =>
-    this.isThreeInARow(this.grid[0][col], this.grid[1][col], this.grid[2][col]);
+  private isWinnerInColumn(col: number) {
+    return this.isThreeInARow(
+      this.grid[0][col],
+      this.grid[1][col],
+      this.grid[2][col],
+    );
+  }
 
-  private isWinnerInBackDiagonal = () =>
-    this.isThreeInARow(this.grid[0][0], this.grid[1][1], this.grid[2][2]);
+  private isWinnerInBackDiagonal() {
+    return this.isThreeInARow(
+      this.grid[0][0],
+      this.grid[1][1],
+      this.grid[2][2],
+    );
+  }
 
-  private isWinnerInForwardDiagonal = () =>
-    this.isThreeInARow(this.grid[0][2], this.grid[1][1], this.grid[2][0]);
+  private isWinnerInForwardDiagonal() {
+    return this.isThreeInARow(
+      this.grid[0][2],
+      this.grid[1][1],
+      this.grid[2][0],
+    );
+  }
 
-  private isFull = () =>
-    this.grid
+  private isFull() {
+    return this.grid
       .map((row) =>
         row.map((sqr) => !!sqr.name).reduce((acc, cur) => acc && cur),
       )
       .reduce((acc, cur) => acc && cur);
+  }
 
-  private isThreeInARow = (sq1: Player, sq2: Player, sq3: Player) =>
-    !!sq1.name &&
-    !!sq2.name &&
-    !!sq3.name &&
-    sq1.name === sq2.name &&
-    sq1.name === sq3.name;
+  private isThreeInARow(sq1: Player, sq2: Player, sq3: Player) {
+    return (
+      !!sq1.name &&
+      !!sq2.name &&
+      !!sq3.name &&
+      sq1.name === sq2.name &&
+      sq1.name === sq3.name
+    );
+  }
 }
