ad158f5f93d473af62ae0ceccf307be4e7439e26
Jonathan Howard <jonathanhoward@fastmail.com>
2024-03-19 09:59:56 -0400

    Inline Message

--- src/App/components/Header/Header.tsx
+++ src/App/components/Header/Header.tsx
@@ -1,10 +1,19 @@
+import React from "react";
+import AppContext from "../../AppContext";
 import HidingResetButton from "./HidingResetButton";
-import Message from "./Message";
 
 export default function Header() {
+  const { state } = React.useContext(AppContext);
+
+  const gameStatus = state.winner.name
+    ? state.winner.name === "Tie"
+      ? "Tie!"
+      : `${state.winner.name} wins!`
+    : `${state.player.name}'s turn`;
+
   return (
     <div className="Header">
-      <Message />
+      <div className="gameStatus">{gameStatus}</div>
       <HidingResetButton />
     </div>
   );
