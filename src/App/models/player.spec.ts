295493be23dd2f7d4f3aa4ab70b4bb04e898e549
Jonathan Howard <jonathanhoward@fastmail.com>
2024-04-11 14:32:54 -0400

    Rewrote test to assert behavior of Player only

--- src/App/models/player.spec.ts
+++ src/App/models/player.spec.ts
@@ -1,17 +1,17 @@
+import { Coordinate } from "../types";
 import Board from "./board";
 import { Player } from "./player";
 
 describe("placeToken", () => {
   it("should place the player's token on the board", () => {
     const player = new Player({ name: "", image: "" });
-    const board = Board.create();
+    const board = {
+      add: jest.fn() as jest.Mocked<typeof Board.prototype.add>,
+    } as Board;
+    const position: Coordinate = [0, 0];
 
-    player.placeToken(board, [0, 0]);
+    player.placeToken(board, position);
 
-    expect(board.grid).toEqual([
-      [player.token, null, null],
-      [null, null, null],
-      [null, null, null],
-    ]);
+    expect(board.add).toHaveBeenCalledWith(player.token, position);
   });
 });
