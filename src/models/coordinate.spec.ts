b8e798fb2059f420579929f4d8b9e1dc173e005c
Jonathan Howard <Jonathan.Howard@insight.com>
2025-11-04 15:25:58 -0500

    Refactor tests

--- src/models/coordinate.spec.ts
+++ src/models/coordinate.spec.ts
@@ -17,12 +17,16 @@ describe("Coordinate", () => {
     expect(a.equals(b)).toBe(false);
   });
 
-  it("matches returns true when row and col match, false otherwise", () => {
+  it("matches returns true when row and col match", () => {
     const c = new Coordinate(2, 1);
 
     expect(c.matches(2, 1)).toBe(true);
+  });
+
+  it("matches returns false when row and col do not match", () => {
+    const c = new Coordinate(2, 1);
+
     expect(c.matches(1, 2)).toBe(false);
-    expect(c.matches(2, 0)).toBe(false);
   });
 
   it("throws for non-integer coordinates", () => {
@@ -30,9 +34,12 @@ describe("Coordinate", () => {
     expect(() => new Coordinate(0, 1.2)).toThrow();
   });
 
-  it("throws for out-of-bounds coordinates", () => {
-    expect(() => new Coordinate(-1, 0)).toThrow();
-    expect(() => new Coordinate(0, 3)).toThrow();
-    expect(() => new Coordinate(3, 3)).toThrow();
+  it.each([
+    [-1, 0],
+    [0, -1],
+    [0, 3],
+    [3, 0],
+  ])("throws for out-of-bounds coordinates", (row, col) => {
+    expect(() => new Coordinate(row, col)).toThrow();
   });
 });
