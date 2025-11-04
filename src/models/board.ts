fd42aebbb8adb419e99f9d0c52b926dcd4926fbe
Jonathan Howard <Jonathan.Howard@insight.com>
2025-11-04 14:28:58 -0500

    Implement winner detection logic in Board class and simplify Game's checkForWinner method

--- src/models/board.ts
+++ src/models/board.ts
@@ -1,5 +1,6 @@
 import { Coordinate, Grid } from "../types";
 import { Player } from "./player";
+import { tie } from "../constants/players";
 
 export class Board {
   readonly #grid: Grid;
@@ -35,5 +36,66 @@ export class Board {
 
   isOccupiedAt(position: Coordinate): boolean {
     return !!this.getAt(position);
+  }
+
+  // Determine if there is a winner or a tie on this board
+  winner(): Player | null {
+    for (let i = 0; i < 3; ++i) {
+      if (this.#isWinnerInRow(i) || this.#isWinnerInColumn(i)) {
+        return this.getAt(new Coordinate(i, i));
+      }
+    }
+
+    if (this.#isWinnerInBackDiagonal() || this.#isWinnerInForwardDiagonal()) {
+      return this.getAt(new Coordinate(1, 1));
+    }
+
+    if (this.#isFull()) {
+      return tie;
+    }
+
+    return null;
+  }
+
+  #isWinnerInRow(row: number) {
+    return this.#isThreeInARow(
+      this.getAt(new Coordinate(row, 0)),
+      this.getAt(new Coordinate(row, 1)),
+      this.getAt(new Coordinate(row, 2)),
+    );
+  }
+
+  #isWinnerInColumn(col: number) {
+    return this.#isThreeInARow(
+      this.getAt(new Coordinate(0, col)),
+      this.getAt(new Coordinate(1, col)),
+      this.getAt(new Coordinate(2, col)),
+    );
+  }
+
+  #isWinnerInBackDiagonal() {
+    return this.#isThreeInARow(
+      this.getAt(new Coordinate(0, 0)),
+      this.getAt(new Coordinate(1, 1)),
+      this.getAt(new Coordinate(2, 2)),
+    );
+  }
+
+  #isWinnerInForwardDiagonal() {
+    return this.#isThreeInARow(
+      this.getAt(new Coordinate(0, 2)),
+      this.getAt(new Coordinate(1, 1)),
+      this.getAt(new Coordinate(2, 0)),
+    );
+  }
+
+  #isFull() {
+    return this.rows()
+      .flat()
+      .every((square) => !!square);
+  }
+
+  #isThreeInARow(sq1: Player | null, sq2: Player | null, sq3: Player | null) {
+    return !!sq1 && sq1 === sq2 && sq1 === sq3;
   }
 }
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,7 +1,6 @@
 import { lilo, stitch, tie } from "../constants/players";
 import { Board } from "./board";
 import { Player } from "./player";
-import { Coordinate } from "../types";
 
 export class Game {
   nextPlayer(currentPlayer: Player): Player {
@@ -17,64 +16,6 @@ export class Game {
   }
 
   checkForWinner(board: Board): Player | null {
-    for (let i = 0; i < 3; ++i) {
-      if (this.#isWinnerInRow(board, i) || this.#isWinnerInColumn(board, i)) {
-        return board.getAt(new Coordinate(i, i));
-      }
-    }
-
-    // prettier-ignore
-    if (this.#isWinnerInBackDiagonal(board) || this.#isWinnerInForwardDiagonal(board)) {
-      return board.getAt(new Coordinate(1, 1));
-    }
-
-    if (this.#isFull(board)) {
-      return tie;
-    }
-
-    return null;
-  }
-
-  #isWinnerInRow(board: Board, row: number) {
-    return this.#isThreeInARow(
-      board.getAt(new Coordinate(row, 0)),
-      board.getAt(new Coordinate(row, 1)),
-      board.getAt(new Coordinate(row, 2)),
-    );
-  }
-
-  #isWinnerInColumn(board: Board, col: number) {
-    return this.#isThreeInARow(
-      board.getAt(new Coordinate(0, col)),
-      board.getAt(new Coordinate(1, col)),
-      board.getAt(new Coordinate(2, col)),
-    );
-  }
-
-  #isWinnerInBackDiagonal(board: Board) {
-    return this.#isThreeInARow(
-      board.getAt(new Coordinate(0, 0)),
-      board.getAt(new Coordinate(1, 1)),
-      board.getAt(new Coordinate(2, 2)),
-    );
-  }
-
-  #isWinnerInForwardDiagonal(board: Board) {
-    return this.#isThreeInARow(
-      board.getAt(new Coordinate(0, 2)),
-      board.getAt(new Coordinate(1, 1)),
-      board.getAt(new Coordinate(2, 0)),
-    );
-  }
-
-  #isFull(board: Board) {
-    return board
-      .rows()
-      .flat()
-      .every((square) => !!square);
-  }
-
-  #isThreeInARow(sq1: Player | null, sq2: Player | null, sq3: Player | null) {
-    return !!sq1 && sq1 === sq2 && sq1 === sq3;
+    return board.winner();
   }
 }
